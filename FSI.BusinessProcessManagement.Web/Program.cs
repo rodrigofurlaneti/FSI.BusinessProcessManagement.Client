d523836bb6d1569c213ad44a483fc3b0fd08cead
Rodrigo Luiz Madeira Furlaneti <rodrigofurlaneti31@hotmail.com>
2025-10-21 20:09:33 -0300

    Added

--- FSI.BusinessProcessManagement.Web/Program.cs
+++ FSI.BusinessProcessManagement.Web/Program.cs
@@ -1,36 +1,31 @@
 using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.Extensions.Options;
-using FSI.BusinessProcessManagement.Web;
-using FSI.BusinessProcessManagement.Services;
+using FSI.BusinessProcessManagement.Services;               // se você separou Services
 using FSI.BusinessProcessManagement.Services.Auth;
 using FSI.BusinessProcessManagement.Services.Http;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// opções da API
-builder.Services.Configure<ApiOptions>(builder.Configuration.GetSection("Api"));
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddAuthorizationCore();
 
 builder.Services.AddBlazoredLocalStorage();
+
+// Auth + HTTP
+builder.Services.AddScoped<TokenAccessor>();
+builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthHeaderHandler>();
+builder.Services.AddHttpClient<ApiClient>(http =>
+{
+    http.BaseAddress = new Uri(builder.Configuration["Api:BaseUrl"]!);
+}).AddHttpMessageHandler<AuthHeaderHandler>();
+
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<ProcessService>();
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<RoleService>();
 builder.Services.AddScoped<DepartmentService>();
-builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
-builder.Services.AddScoped<TokenAccessor>();
-
-builder.Services.AddScoped<AuthHeaderHandler>();
-
-builder.Services.AddHttpClient<ApiClient>((sp, http) =>
-{
-    var api = sp.GetRequiredService<IOptions<ApiOptions>>().Value;
-    http.BaseAddress = new Uri(api.BaseUrl);
-}).AddHttpMessageHandler<AuthHeaderHandler>();
 
 var app = builder.Build();
 
@@ -42,10 +37,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
+
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
-
